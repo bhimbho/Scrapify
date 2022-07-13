e787b9d8c521a4a7e7473d95df6b373627acae45
Soneye Sina <advancoplanet@gmail.com>
2022-07-13 13:01:12 +0100

    Error Fixed undefined var

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (isset($argv[1])) {
      * @var Scrapify $scrapy
      */
     //  To run on command line use "php index.php {search Parameter}"
-    $scrapy = new Scrapify($searchParam1);
+    $scrapy = new Scrapify($searchParam);
     print_r($scrapy->extract());
 } else {
     echo 'Kindly pass a seach value';
